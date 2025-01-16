8f3f2a570dccce00141396b72443d4e56c944780
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2025-01-16 15:41:11 -0500

    Add logging for distls special boundaries

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -672,6 +672,7 @@ def main(
         distls = DistLS(boundaries=tokenizer.boundaries, 
                         variance=distls_variance,
                         special_tokens=[pad_token_id, eos_token_id, -100])
+        log_on_main(f"Distls special boundaries: " + str(distls.special_boundaries.shape), logger)
 
     shuffled_train_dataset = ChronosDataset(
         datasets=train_datasets,
