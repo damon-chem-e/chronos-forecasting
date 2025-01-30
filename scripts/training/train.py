50e060d5aeb47db422d1e115e1772f4498ce9dc9
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2025-01-29 21:14:56 -0500

    Log current prediction length

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -566,6 +566,7 @@ def main(
     top_p: float = 1.0,
     seed: Optional[int] = None,
 ):
+    log_on_main(f"Using prediction length: {prediction_length}", logger)
     if tf32 and not (
         torch.cuda.is_available() and torch.cuda.get_device_capability()[0] >= 8
     ):
