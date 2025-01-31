380f5f512d49d522529fe3fe6079f61575fc35c9
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2025-01-31 15:14:20 -0500

    Argmax over correct dimension for debug

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -525,7 +525,7 @@ class DistLSTrainer(Trainer):
         
         if is_main_process() and not hasattr(self, 'printed_once'):
             torch.set_printoptions(profile='full')
-            print("PROBABILITIES: ", probs.shape, torch.argmax(probs, dim=-1))
+            print("PROBABILITIES: ", probs.shape, torch.argmax(probs, dim=-2))
             print("LABELS: ", inputs.get('labels').shape, inputs.get('labels'))
             torch.set_printoptions(profile='default')
             self.printed_once = True
