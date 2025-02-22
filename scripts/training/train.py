c80e6f77db7ae311083cb6f814128e9185fc86c2
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2025-02-22 13:18:39 -0500

    Add chronos bolt config to train.py

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -46,7 +46,7 @@ from gluonts.transform import (
     LastValueImputation,
 )
 
-# from chronos import ChronosConfig, ChronosTokenizer
+from chronos import ChronosConfig, ChronosTokenizer
 from src.chronos.chronos_bolt import ChronosBoltConfig
 
 app = typer.Typer(pretty_exceptions_enable=False)
@@ -650,25 +650,25 @@ def main(
         use_reg_token=use_reg_token
     )
     
-    # chronos_config = ChronosConfig(
-    #     tokenizer_class=tokenizer_class,
-    #     tokenizer_kwargs=tokenizer_kwargs,
-    #     n_tokens=n_tokens,
-    #     n_special_tokens=n_special_tokens,
-    #     pad_token_id=pad_token_id,
-    #     eos_token_id=eos_token_id,
-    #     use_eos_token=use_eos_token,
-    #     model_type=model_type,
-    #     context_length=context_length,
-    #     prediction_length=prediction_length,
-    #     num_samples=num_samples,
-    #     temperature=temperature,
-    #     top_k=top_k,
-    #     top_p=top_p,
-    # )
+    chronos_config = ChronosConfig(
+        tokenizer_class=tokenizer_class,
+        tokenizer_kwargs=tokenizer_kwargs,
+        n_tokens=n_tokens,
+        n_special_tokens=n_special_tokens,
+        pad_token_id=pad_token_id,
+        eos_token_id=eos_token_id,
+        use_eos_token=use_eos_token,
+        model_type=model_type,
+        context_length=context_length,
+        prediction_length=prediction_length,
+        num_samples=num_samples,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+    )
 
     # Add extra items to model config so that it's saved in the ckpt
-    model.config.chronos_config = chronos_config.__dict__
+    model.config.chronos_config = chronos_bolt_config.__dict__
 
     shuffled_train_dataset = ChronosDataset(
         datasets=train_datasets,
